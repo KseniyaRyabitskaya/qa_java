242fd1d8dd22bd51323ae42d53584274fb3ab824
KseniyaRyabitskaya <kseniyanaidionova@mail.ru>
2025-04-05 14:32:47 +0300

    Commit Tests

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,8 +5,10 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
+    private final FelineInterface felineInterface;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, FelineInterface felineInterface) throws Exception {
+        this.felineInterface=felineInterface;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -16,10 +18,8 @@ public class Lion {
         }
     }
 
-    Feline feline = new Feline();
-
     public int getKittens() {
-        return feline.getKittens();
+        return felineInterface.getKittens();
     }
 
     public boolean doesHaveMane() {
@@ -27,6 +27,6 @@ public class Lion {
     }
 
     public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
+        return felineInterface.eatMeat();
     }
 }
